051ad386e73f983c215a452560b22faa82049122
Matheus Reis <souzareis42@gmail.com>
2026-09-10 19:10:40 -0300

    learn: pagina de cadastro com tabela de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Cadastrar } from './cadastrar/cadastrar'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "cadastrar", component:Cadastrar},
+    {path: "", component:Cadastrar}
+];
---
+++ src/app/cadastrar/cadastrar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cadastrar',
+  styleUrl: './cadastrar.css',
+  templateUrl: './cadastrar.html',
+})
+export class Cadastrar {}
